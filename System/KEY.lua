4fa6a3aa58e23b9998ef03a2493a5865ce485c71
Akramer3k <alex_kramer@Me.com>
2023-04-25 19:54:53 -0400

    e

--- System/KEY.lua
+++ System/KEY.lua
@@ -3,7 +3,7 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shl
 local Window = OrionLib:MakeWindow({Name = "Ak's Script hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Orion"})
 
 local Tab = Window:MakeTab({
-	Name = "Tab 1",
+	Name = "Key",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
